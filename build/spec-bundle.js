c8be0b2a3d2df59b980fbef4866f0b634d70367e
polang <grzx2210@163.com>
2017-05-08 21:09:19 +0800

    [rebirth-http] fix test

--- build/spec-bundle.js
+++ build/spec-bundle.js
@@ -1,3 +1,7 @@
+/**
+ * @author: @AngularClass
+ */
+
 /*
  * When testing with webpack and ES6, we have to do some extra
  * things to get testing to work right. Because we are gonna write tests
@@ -12,19 +16,32 @@ Error.stackTraceLimit = Infinity;
 require('core-js/es6');
 require('core-js/es7/reflect');
 
-// Typescript emit helpers polyfill
-require('ts-helpers');
+require('zone.js/dist/zone');
+require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy'); // since zone.js 0.6.15
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
+require('zone.js/dist/async-test');
+require('zone.js/dist/fake-async-test');
 
 // RxJS
 require('rxjs/Rx');
 
+var testing = require('@angular/core/testing');
+var browser = require('@angular/platform-browser-dynamic/testing');
+
+testing.TestBed.initTestEnvironment(
+    browser.BrowserDynamicTestingModule,
+    browser.platformBrowserDynamicTesting()
+);
+
 /*
- * Ok, this is kinda crazy. We can use the the context method on
+ * Ok, this is kinda crazy. We can use the context method on
  * require that webpack created in order to tell webpack
  * what files we actually want to require or import.
- * Below, context will be an function/object with file names as keys.
- * using that regex we are saying look in ./src/app and ./test then find
- * any file that ends with spec.js and get its path. By passing in true
+ * Below, context will be a function/object with file names as keys.
+ * Using that regex we are saying look in ../src then find
+ * any file that ends with spec.ts and get its path. By passing in true
  * we say do this recursively
  */
 var testContext = require.context('../src', true, /\.spec\.ts/);
